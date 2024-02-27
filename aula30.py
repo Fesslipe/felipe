a142e03ff5fe31f0d7e73b0f6004a0d83c72e9c8
Fesslipe <119904645+Fesslipe@users.noreply.github.com>
2024-02-27 10:49:58 -0300

    Update aula30.py

--- aula30.py
+++ aula30.py
@@ -1,17 +1,53 @@
-from flask import Flask, request, make_response, redirect, abort, render_template, request
-app = Flask(__name__);
+from flask import Flask, request
+
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template("index.html");
+def home():
+    return '''
+        <html>
+            <body>
+                <h1>Avaliação contínua: Aula 030</h1>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/identificacao">Identificação</a></li>
+                    <li><a href="/contexto_requisicao">Contexto da Requisição</a></li>
+                </ul>
+            </body>
+        </html>
+    '''
 
-@app.route('/user/<name>/<enrollment>/<college>')
-def user(name, enrollment, college):
-    return render_template("user.html").format(name, enrollment, college);
+@app.route('/identificacao/<nome>/<prontuario>/<instituicao>')
+@app.route('/identificacao')
+def identificacao(nome="Felipe", prontuario="3020673", instituicao="IFSP"):
+    return '''
+        <html>
+            <body>
+                <h1>Avaliação contínua: Aula 030</h1>
+                <h2>Aluno: {nome}</h2>
+                <h2>Prontuário: {prontuario}</h2>
+                <h2>Instituição: {instituicao}</h2>
+                <p><a href="/">Voltar</a></p>
+            </body>
+        </html>
+    '''.format(nome=nome, prontuario=prontuario, instituicao=instituicao)
 
-@app.route('/contextorequisicao')
-def requestContext():
-    userAgent = request.headers.get("User-Agent")
-    userIp = request.headers.get("host")
-    appHost = request.headers.get("Referer")
-    return render_template("requestContext.html").format(userAgent, userIp, appHost)
+@app.route('/contexto_requisicao')
+def contexto_requisicao():
+    user_agent = request.headers.get('User-Agent')
+    remote_addr = request.remote_addr
+    host = request.host
+    return '''
+        <html>
+            <body>
+                <h1>Avaliação contínua: Aula 030</h1>
+                <h2>Seu navegador é: {user_agent}</h2>
+                <h2>O IP do computador remoto é: {remote_addr}</h2>
+                <h2>O host da aplicação é: {host}</h2>
+                <p><a href="/">Voltar</a></p>
+            </body>
+        </html>
+    '''.format(user_agent=user_agent, remote_addr=remote_addr, host=host)
+
+if __name__ == '__main__':
+    app.run(debug=True)
